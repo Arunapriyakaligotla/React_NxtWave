e49f492db917cea79c4fb7d2669ed4e38f9a8f0b
Arunapriyakaligotla <159990605+Arunapriyakaligotla@users.noreply.github.com>
2024-05-21 22:54:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ListView from "./Components/ListView";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div>
+      <ListView />
+    </div>
+  );
+}
+
+export default App;
